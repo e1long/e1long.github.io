26b40903bf5af8ebf83e604106c8b80e4f6b9836
emclysm <e1long@torontomu.ca>
2024-10-04 00:03:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("nameButton").addEventListener("click", function() {
-        const name = prompt("Please enter your name:");
-        
-        if (name) {
-            document.getElementById("nameOutput").innerText = "Hello, " + name + "!";
-        } else {
-            document.getElementById("nameOutput").innerText = "No name entered.";
-        }
-    });
+//JavaScript
+document.getElementById("nameButton").addEventListener("click", function() {
+    const name = prompt("Please enter your name:");
+    if (name) {
+        document.getElementById("nameOutput").innerText = "Hello, " + name + "!";
+    }
 });
