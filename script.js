5793143b434f588742fa75838013750cc7611c30
emclysm <e1long@torontomu.ca>
2024-10-04 10:57:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 document.getElementById("nameButton").addEventListener("click", function() {
     const name = prompt("Please enter your name:");
     if (name) {
-        document.getElementById("nameOutput").innerText = "Hello, " + name + "!";
+        document.getElementById("nameOutput").innerText = "Hello, " + name + ",";
     }
 });
