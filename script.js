ce6a0fda975c4eb57a31f8b297877abe923a6a9f
emclysm <e1long@torontomu.ca>
2024-10-04 16:05:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+//JavaScript
+document.getElementById("nameButton").addEventListener("click", function() {
+    const name = prompt("Please enter your name:");
+    if (name) {
+        document.getElementById("nameOutput").innerText = "Hello, " + name + ",";
+    }
+});
+
+document.getElementById("myImage").addEventListener("click", function() {
+    this.style.width = "400px";
+});
+
+document.getElementById("myImage").addEventListener("mouseout", function() {
+    this.style.width = "200px";
+});
